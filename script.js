01554aeebe8aac0604838010c86625c0d74e5009
nini1504 <nicolly.luz@ufu.br>
2024-10-03 11:36:05 -0300

    Inicio

---
+++ script.js
@@ -0,0 +1,8 @@
+function conferirSenha() {
+    window.location.href = "home.html"; // Redireciona para a página home
+}
+
+// Função para redirecionar para a página de cadastro
+function cadastrarCliente() {
+    window.location.href = "cadastro.html"; // Redireciona para a página de cadastro
+}
