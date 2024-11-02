f5770b0c2b2da186c418b7c499e3122b4fb73c20
camillyml <camillymlcruvinel@gmail.com>
2024-11-02 18:25:00 -0300

    alteração página prato

--- pratos.js
+++ pratos.js
@@ -1,22 +1,7 @@
-const apiUrlPratos = 'http://localhost:8080/pratosAvaliados';
-
-async function carregarPratos() {
-    try {
-        const response = await fetch(apiUrlPratos);
-        const pratos = await response.json();
-
-        const pratosDiv = document.getElementById('pratos');
-        pratosDiv.innerHTML = pratos.map(prato => `
-            <div class="prato">
-                <h2>${prato.nome}</h2>
-                <p>Avaliação: ${prato.avaliacao} estrelas</p>
-                <p>${prato.descricao}</p>
-            </div>
-        `).join('');
-    } catch (error) {
-        console.error('Erro ao carregar pratos:', error);
-        document.getElementById('pratos').textContent = 'Erro ao carregar pratos.';
-    }
+function adicionarPrato() {
+    alert("Função restrita a funcionários. Você não tem permissão para adicionar pratos.");
 }
 
-window.onload = carregarPratos;
+function removerPrato() {
+    alert("Função restrita a funcionários. Você não tem permissão para excluir pratos.");
+}
