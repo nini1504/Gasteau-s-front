c7417d9609cab37b472cbfc32811488123efbbd8
Lavínia Barbosa Dantas de Souza <laviniabd.barbosa@gmail.com>
2024-11-04 12:21:26 -0300

    Cadastro Funcionario OK

--- cadastroFuncionario.js
+++ cadastroFuncionario.js
@@ -1,22 +1,25 @@
-
-const apiUrlCadastroFuncionario = 'http://localhost:8080/cadastro/admin';
+const apiUrlCadastroFuncionario = 'http://localhost:8080/cadastro/funcionario';
 
 async function cadastrarFuncionario() {
-    const nome = document.getElementById('nomeFuncionario').value;
-    const nrocarteira = document.getElementById('nrocarteira').value;
-    const cargo = document.getElementById('cargoFuncionario').value;
-    const senha = document.getElementById('senhaFuncionario').value;
+    const nome = document.getElementById('nome').value; 
+    const nrocarteira = document.getElementById('nrocarteira').value; 
+    const cargo = document.getElementById('cargo').value; 
+    const telefone = document.getElementById('telefone').value; // Novo campo
+    const dataContratacao = document.getElementById('dataContratacao').value; // Novo campo
+    const senha = document.getElementById('senha').value; 
     const curriculo = document.getElementById('curriculoFuncionario').files[0];
 
-    if (!nome || !nrocarteira || !cargo || !senha || !curriculo) {
+    if (!nome || !nrocarteira || !cargo || !telefone || !dataContratacao || !senha) { // Adicionei as novas validações
         alert('Por favor, preencha todos os campos!');
         return;
     }
 
     const formData = new FormData();
     formData.append('nome', nome);
-    formData.append('nrocarteira', nrocarteira);
-    formData.append('cargo', cargo);
+    formData.append('nroCarteira', nrocarteira);
+    formData.append('funcao', cargo);
+    formData.append('telefone', telefone); // Adicionado
+    formData.append('dataContratacao', dataContratacao); // Adicionado
     formData.append('senha', senha);
     formData.append('curriculo', curriculo);
 
@@ -29,12 +32,8 @@ async function cadastrarFuncionario() {
         if (!response.ok) throw new Error('Erro ao cadastrar funcionário');
         alert('Cadastro de funcionário realizado com sucesso!');
 
-        // Armazena o nrocarteira no localStorage 
         localStorage.setItem("nrocarteira", nrocarteira);
-
-        // Redireciona para a home
-        window.location.href = "home.html";//ver se pode redirecionar para a página de dados dele
-
+        window.location.href = "home.html";
     } catch (error) {
         console.error('Erro ao cadastrar funcionário:', error);
         alert('Erro ao realizar cadastro.');
